899c39d5ddb651310e09114ce46f76e2a94f0101
Hernan Pieroni <hernan.pieroni@shift.com>
2021-09-10 14:56:11 -0300

    feat: implement SnakeToCamelKeys type utility

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,1 +1,10 @@
 export type AnyObject = Record<string, unknown>;
+
+type SnakeToCamelCase<S extends string> =
+  S extends `${infer A}_${infer B}${infer C}`
+    ? `${Lowercase<A>}${Capitalize<B>}${SnakeToCamelCase<C>}`
+    : Lowercase<S>;
+
+export type SnakeToCamelKeys<T extends AnyObject> = {
+  [K in keyof T as SnakeToCamelCase<K & string>]: T[K];
+};
