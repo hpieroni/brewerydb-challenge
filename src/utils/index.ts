67eaad5f194cbc672fa10e0199838f199a9d0be6
Hernan Pieroni <hernan.pieroni@shift.com>
2021-09-10 14:56:39 -0300

    feat: add pipe function

---
+++ src/utils/index.ts
@@ -0,0 +1,17 @@
+import capitalize from './capitalize';
+import map from './map';
+import mapKeys from './mapKeys';
+import pipe from './pipe';
+import removeNulls from './removeNulls';
+import removePropsWith from './removePropsWith';
+import snakeToCamel from './snakeToCamel';
+
+export {
+  capitalize,
+  map,
+  mapKeys,
+  pipe,
+  removeNulls,
+  removePropsWith,
+  snakeToCamel,
+};
---
+++ src/utils/pipe.ts
@@ -0,0 +1,30 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+
+/**
+ * Compose functions from right to left
+ *
+ *
+ * @param {...Func} num - A positive or negative number.
+ * @returns {Func} The composed Function.
+ */
+function pipe<V extends any[], T1>(fn0: (...fns: V) => T1): (...fns: V) => T1;
+function pipe<V extends any[], T1, T2>(
+  fn0: (...fns: V) => T1,
+  fn1: (x: T1) => T2
+): (...fns: V) => T2;
+function pipe<V extends any[], T1, T2, T3>(
+  fn0: (...fns: V) => T1,
+  fn1: (x: T1) => T2,
+  fn2: (x: T2) => T3
+): (...fns: V) => T2;
+function pipe<V extends any[], T1, T2, T3, T4>(
+  fn0: (...fns: V) => T1,
+  fn1: (x: T1) => T2,
+  fn2: (x: T2) => T3,
+  fn3: (x: T3) => T4
+): (...fns: V) => T2;
+function pipe(...fns: any[]): any {
+  return (value: any) => fns.reduce((y, f) => f(y), value);
+}
+
+export default pipe;
