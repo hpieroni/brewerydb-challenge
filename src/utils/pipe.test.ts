3854e912a520b79d079f1d501166d56d6f26933e
Hernan Pieroni <hernan.pieroni@shift.com>
2021-09-10 15:22:47 -0300

    test: commit pipe function tests

---
+++ src/utils/pipe.test.ts
@@ -0,0 +1,31 @@
+import pipe from './pipe';
+
+describe('`pipe` function', () => {
+  const length = (str: string) => str.length;
+  const isEven = (n: number) => n % 2 === 0;
+  const complement = (bool: boolean) => !bool;
+
+  test('should work with one function', () => {
+    const pipedFn = pipe(length);
+
+    expect(pipedFn('hi')).toBe(2);
+    expect(pipedFn('hello')).toBe(5);
+  });
+
+  test('should work with multiple functions', () => {
+    const pipedFn = pipe(length, isEven, complement);
+
+    expect(pipedFn('hi')).toBe(false);
+    expect(pipedFn('hello')).toBe(true);
+  });
+
+  test('should be transitive', () => {
+    const piped1 = pipe(length, isEven, complement);
+    const piped2 = pipe(length, pipe(isEven, complement));
+    const piped3 = pipe(pipe(length, isEven), complement);
+
+    expect(piped1('hi')).toBe(false);
+    expect(piped2('hi')).toBe(false);
+    expect(piped3('hi')).toBe(false);
+  });
+});
